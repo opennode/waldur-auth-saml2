82b1e8742e640f8225be349334d00ed09ec2e272
Jenkins <jenkins@opennodecloud.com>
2015-11-22 13:45:31 +0000

    Preparing new release: 0.1.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ install_requires = [
 
 setup(
     name='nodeconductor-saml2',
-    version='0.1.2',
+    version='0.1.3',
     author='OpenNode Team',
     author_email='info@opennodecloud.com',
     url='http://nodeconductor.com',

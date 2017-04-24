dfb1a900edb937b4d630b313583195c20b5db55f
Victor Mireyev <victor@opennodecloud.com>
2017-04-24 13:32:09 +0300

    Sync login completed URL template with HomePort.

--- src/nodeconductor_saml2/extension.py
+++ src/nodeconductor_saml2/extension.py
@@ -47,7 +47,7 @@ class SAML2Extension(NodeConductorExtension):
             'organization': {},
 
             # Callback URL
-            'LOGIN_COMPLETED_URL': 'http://example.com/#/login_completed/{token}/',
+            'LOGIN_COMPLETED_URL': 'http://example.com/#/login_complete/{token}/',
             'LOGIN_FAILED_URL': 'http://example.com/#/login_failed/',
             'LOGOUT_COMPLETED_URL': 'http://example.com/#/logout_completed/',
             'LOGOUT_FAILED_URL': 'http://example.com/#/logout_failed/',

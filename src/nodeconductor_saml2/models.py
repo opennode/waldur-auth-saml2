4a8848f7d688b5693168a18a6bbc7cfd9cff36cb
Victor Mireyev <victor@opennodecloud.com>
2017-06-13 19:41:56 +0300

    Import SAML2 identity providers metadata in the database [WAL-945]

--- src/nodeconductor_saml2/models.py
+++ src/nodeconductor_saml2/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
+from nodeconductor.core import fields as core_fields
+
 
 class IdentityProvider(models.Model):
     name = models.TextField(db_index=True)
     url = models.URLField()
+    metadata = core_fields.JSONField(default={})
 
     class Meta(object):
         ordering = ('name',)
--- src/nodeconductor_saml2/tasks.py
+++ src/nodeconductor_saml2/tasks.py
@@ -1,24 +1,43 @@
 from celery import shared_task
-
-from djangosaml2.conf import get_config
-from djangosaml2.utils import available_idps
+from django.conf import settings
+from saml2.attribute_converter import ac_factory
+from saml2.mdstore import MetaDataFile, name as get_idp_name
 
 from . import models
 
 
+def load_metadata():
+    metadata = {}
+    for filename in settings.NODECONDUCTOR_SAML2['idp_metadata_local']:
+        mdf = MetaDataFile(ac_factory(), filename)
+        mdf.load()
+        metadata.update(mdf.items())
+    return metadata
+
+
 @shared_task(name='nodeconductor_saml2.sync_providers')
 def sync_providers():
-    current_idps = list(models.IdentityProvider.objects.all().only('url', 'pk'))
-    backend_idps = available_idps(get_config())
+    mdf = load_metadata()
 
-    backend_urls = set(backend_idps.keys())
+    current_idps = list(models.IdentityProvider.objects.all().only('url', 'pk'))
+    backend_urls = set(mdf.keys())
+
     stale_idps = set(idp.pk for idp in current_idps if idp.url not in backend_urls)
     models.IdentityProvider.objects.filter(pk__in=stale_idps).delete()
 
     existing_urls = set(idp.url for idp in current_idps)
-    missing_idps = [
-        models.IdentityProvider(url=url, name=name)
-        for (url, name) in backend_idps.items()
-        if name and url not in existing_urls
-    ]
+    missing_idps = []
+
+    for url, metadata in mdf.items():
+        name = get_idp_name(metadata)
+        if not name:
+            # It is expected that every provider has name.
+            # Skip invalid identity provider
+            continue
+        if url in existing_urls:
+            # Skip identity provider if its url is already in the database
+            continue
+        missing_idps.append(
+            models.IdentityProvider(url=url, name=name, metadata=metadata)
+        )
     models.IdentityProvider.objects.bulk_create(missing_idps)

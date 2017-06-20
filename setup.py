8ab6c71ad2f22a02d84fc08a1912f8f580eb3940
Jenkins <jenkins@opennodecloud.com>
2017-06-20 18:54:47 +0000

    Preparing new release: 0.6.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ install_requires = [
 
 setup(
     name='nodeconductor-saml2',
-    version='0.5.4',
+    version='0.6.0',
     author='OpenNode Team',
     author_email='info@opennodecloud.com',
     url='http://waldur.com',
